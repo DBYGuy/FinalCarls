8dffa7e405cec04b1b556359fc0f195011f506dd
Adam Flaum <adam@dbyclub.xyz>
2023-12-31 11:04:56 -0800

    updating app APIs

--- src/hooks/useGetLevel.ts
+++ src/hooks/useGetLevel.ts
@@ -3,6 +3,7 @@ import { useMe } from './useMe';
 
 export function useGetLevel() {
   const user = useMe();
+  const userId = user?.id ?? '';
 
   // Using tRPC's useQuery hook to fetch user level
   const {
@@ -10,12 +11,9 @@ export function useGetLevel() {
     isLoading,
     isError,
     error,
-  } = trpc.level.getUserLevel.useQuery(
-    user?.id ?? '', // Pass user ID directly as a string, or an empty string if undefined
-    {
-      enabled: !!user?.id, // Only run the query if the user ID is available
-    },
-  );
+  } = trpc.level.getUserLevel.useQuery(userId, {
+    enabled: !!user,
+  });
 
   return {
     level,
--- src/hooks/useGetUserRewards.ts
+++ src/hooks/useGetUserRewards.ts
@@ -2,6 +2,7 @@ import { trpc } from '../utils/trpc';
 
 export const useGetUserReward = (userId: string) => {
   const query = trpc.rewards.getUserReward.useQuery(userId, {
+    enabled: !!userId,
     refetchOnWindowFocus: true,
     refetchOnMount: false,
   });
