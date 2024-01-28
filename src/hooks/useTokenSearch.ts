e79e53ad241ace4c1129d953fe71eabfa433cf04
Adam Flaum <adam@dbyclub.xyz>
2024-01-28 14:11:57 -0800

    Update hooks for directory

--- src/hooks/useTokenSearch.ts
+++ src/hooks/useTokenSearch.ts
@@ -1,21 +1,28 @@
 import { useState, useEffect } from 'react';
 import { trpc } from '~/utils/trpc';
-import type { SearchResult } from '~/components/SearchBar'; // Import SearchResult type
-import useDebounce from '~/hooks/useDebounce'; // Import useDebounce hook
+import type { SearchResult } from '~/components/SearchBar';
+import useDebounce from '~/hooks/useDebounce';
 
 export const useTokenSearch = (searchTerm: string, itemsPerPage = 12) => {
   const [results, setResults] = useState<SearchResult[]>([]);
   const [page, setPage] = useState(0);
   const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
-  const { data, isLoading, isError } = trpc.token.search.useQuery({
-    query: debouncedSearchTerm || undefined,
-    page,
-    itemsPerPage,
-  });
+  const { data, isLoading, isError } = trpc.token.search.useQuery(
+    {
+      query: debouncedSearchTerm || '',
+      page,
+      itemsPerPage,
+    },
+    {
+      enabled: true,
+      refetchOnWindowFocus: true,
+      refetchOnMount: true,
+    },
+  );
 
   useEffect(() => {
-    // Reset results and page only when the actual search term changes
+    // Reset results and page when the search term changes
     if (searchTerm !== debouncedSearchTerm) {
       setResults([]);
       setPage(0);
@@ -23,10 +30,9 @@ export const useTokenSearch = (searchTerm: string, itemsPerPage = 12) => {
   }, [searchTerm, debouncedSearchTerm]);
 
   useEffect(() => {
+    // Append new data to existing results, avoiding duplicates
     if (data && !isLoading && !isError) {
-      // Append new data to existing results
       setResults((prevResults) => {
-        // Ensure no duplication of results
         const existingIDs = new Set(
           prevResults.map((result) => result.tokenID),
         );
